ef0e79c8403806a6d0e5c2ad30e3e8d48108c996
lennardrischen <lennard@rischen.de>
2025-05-28 22:00:58 +0200

    Add static variables, function overrides and logs to AppWidgetProvider

--- app/src/main/java/com/lennardrischen/bitcoinwidgets/PriceWidget.kt
+++ app/src/main/java/com/lennardrischen/bitcoinwidgets/PriceWidget.kt
@@ -3,29 +3,50 @@ package com.lennardrischen.bitcoinwidgets
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.widget.RemoteViews
 
 /**
  * Implementation of App Widget functionality.
  */
 class PriceWidget : AppWidgetProvider() {
+    companion object {
+        const val WIDGET_WORK_NAME = "BitcoinPriceUpdateWork"
+        const val TAG = "ChartWidget"
+        const val ACTION_REFRESH_WIDGET = "com.lennardrischen.bitcoinapp.ACTION_REFRESH_WIDGET"
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
+        Log.d(TAG, "onUpdate invoked.")
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
 
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.d(TAG, "onReceive invoked.")
+        super.onReceive(context, intent)
+    }
+
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
+        Log.d(TAG, "onEnabled invoked.")
     }
 
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
+        Log.d(TAG, "onDisabled invoked.")
+    }
+
+    override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
+        Log.d(TAG, "onDeleted invoked.")
+        super.onDeleted(context, appWidgetIds)
     }
 }
 
